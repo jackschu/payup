72ba76affa30d04962a421f63bb3c5e32707770d
jackschu <js9pa@virginia.edu>
2019-03-02 17:28:01 -0500

    adding navigation and firebase

---
+++ payup_mobile/src/screens/AddFriend.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';  
+import { View, Text } from 'react-native';
+
+type Props = {};
+export default class AddFriend extends Component {  
+  render() {
+    return (
+      <View>
+        <Text>Add Item</Text>
+      </View>
+    );
+  }
+}
