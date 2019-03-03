ffcfe13705030c5d45a4b29d149a2496b95f8e23
jackschu <js9pa@virginia.edu>
2019-03-03 10:39:33 -0500

    adding utc to fire

--- payup_mobile/src/screens/GoalsPage.js
+++ payup_mobile/src/screens/GoalsPage.js
@@ -60,7 +60,6 @@ export default class GoalsPage extends Component {
 	    this.handleModalOpen=this.handleModalOpen.bind(this)
 		this.onRegionChange=this.onRegionChange.bind(this)
 		this.addGoalHandler=this.addGoalHandler.bind(this)
-		this.doThing=this.doThing.bind(this)
 
 
 		// this.setState(modalVisible(false))
@@ -68,7 +67,8 @@ export default class GoalsPage extends Component {
     async send_goal(){
 	var user = firebase.auth().currentUser;
 	goal = {}
-	var title = this.state.text	
+	var title = this.state.text
+	goal['utc'] = this.state.utcDate
 	goal['desc'] = this.state.desc
 	goal['lat'] = this.state.markerCoordinates.latitude
 	goal['lon'] = this.state.markerCoordinates.longitude	
@@ -83,12 +83,10 @@ export default class GoalsPage extends Component {
 	addGoalHandler() {
 		var newDate = new Date(this.state.date + " " + this.state.time).getTime();
 		console.log(newDate);
-		this.setState({modalVisible: false, utcDate: newDate}, this.doThing);
-	}
-
-	doThing() {
-		this.send_goal();
-		this.populate_goals(firebase.auth().currentUser.uid);	
+		this.setState({modalVisible: false, utcDate: newDate});
+	 	this.send_goal();
+	    this.populate_goals(firebase.auth().currentUser.uid);
+	    
 	}
 
     
@@ -137,6 +135,7 @@ export default class GoalsPage extends Component {
 	var new_goals= []
 	for (var title in goals_list){
 	    var goal = {}
+	    goal['utc'] = goals_list[title]['utc']
 	    goal['lat'] = goals_list[title]['lat']	
 	    goal['lon'] = goals_list[title]['lon']    
 	    goal['title'] = title
@@ -227,7 +226,7 @@ export default class GoalsPage extends Component {
 		return (
 			<View style={{flex: 1}}>
 
-
+		    	<ScrollView>
 			{this.state.goals.map((goal)=>
 			 (<Card style={cardStyle.cardStyle}>
 			  <Card.Title title={goal.title} subtitle={'Betting ' +goal.amount+ 'USD against '+ goal.friends} left={(props) => <Avatar.Icon {...props} icon="account-circle" />} />
@@ -239,10 +238,12 @@ export default class GoalsPage extends Component {
 			  <Paragraph> {'Penalty for not succeeding ' +goal.amount+"USD"} </Paragraph>
 					</Card.Content>
 					<Card.Actions>
-					<Button>Edit</Button>
+					<Button></Button>
 					</Card.Actions>
 			  </Card>))
+			 
 			}
+		    	</ScrollView>
 				<Modal style={modalStyle.parent} animated={true} visible={this.state.modalVisible} onRequestClose={() => this.setState({ modalVisible: false })}>
 					<ScrollView>
 					<Text style={modalStyle.title}> New Goal </Text>
